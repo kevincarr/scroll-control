cb8b774b134f1b061a6361584d7d7717c11f77c4
kevincarr <kevinmcarr@hotmail.com>
2014-12-18 15:18:21 -0600

    Adds missing objects to array then sorts min/max arrays to so they match.

--- js/scrollControl.js
+++ js/scrollControl.js
@@ -30,65 +30,66 @@ var orgListMax=[];
 i=0;
 orgListMin=[];
 orgListMin.push([0]); // starting pixel
-orgListMin.push([oOrgObjA,300,50,10,100]);
-orgListMin.push([oOrgObjB,550,100,45,100]);
-orgListMin.push([oOrgObjC,300,150,0,100]);
-orgListMin.push([oOrgObjD,400,200,-20,100]);
-orgListMin.push([oOrgObjE,200,250,30,100]);
-orgListMin.push([oOrgObjF,540,300,-30,100]);
-orgListMin.push([oOrgObjG,300,350,0,100]);
-orgListMin.push([oOrgObjH,100,400,-45,100]);
-orgListMin.push([oOrgObjI,400,450,20,100]);
-orgListMin.push([oOrgObjJ,600,500,-10,100]);
+orgListMin.push(['#orgObjA',310,50,10,100]);
+orgListMin.push(['#orgObjB',520,100,45,100]);
+orgListMin.push(['#orgObjC',330,150,0,100]);
+orgListMin.push(['#orgObjD',440,200,-20,100]);
+orgListMin.push(['#orgObjE',250,250,30,100]);
+orgListMin.push(['#orgObjF',560,300,-30,100]);
+orgListMin.push(['#orgObjG',370,350,0,100]);
+orgListMin.push(['#orgObjH',180,400,-45,100]);
+orgListMin.push(['#orgObjI',490,450,20,100]);
+orgListMin.push(['#orgObjJ',600,500,-10,100]);
 orgList[i]=orgListMin;
 // Scene 2
 i++;
 orgListMin=[];
 orgListMin.push([400]); // starting pixel
-orgListMin.push([oOrgObjA,400,600,0,50]);
-orgListMin.push([oOrgObjB,400,600,0,50]);
-orgListMin.push([oOrgObjC,400,600,0,50]);
-orgListMin.push([oOrgObjD,400,600,0,50]);
-orgListMin.push([oOrgObjE,400,600,0,50]);
-orgListMin.push([oOrgObjF,400,600,0,50]);
-orgListMin.push([oOrgObjG,400,600,0,50]);
-orgListMin.push([oOrgObjH,400,600,0,50]);
-orgListMin.push([oOrgObjI,400,600,0,50]);
-orgListMin.push([oOrgObjJ,400,600,0,50]);
+orgListMin.push(['#orgObjA',401,600,0,50]);
+orgListMin.push(['#orgObjB',402,600,0,50]);
+orgListMin.push(['#orgObjC',403,600,0,50]);
+orgListMin.push(['#orgObjD',404,600,0,50]);
+orgListMin.push(['#orgObjE',405,600,0,50]);
+orgListMin.push(['#orgObjF',406,600,0,50]);
+orgListMin.push(['#orgObjG',407,600,0,50]);
+orgListMin.push(['#orgObjH',408,600,0,50]);
+orgListMin.push(['#orgObjI',409,600,0,50]);
+orgListMin.push(['#orgObjJ',400,600,0,50]);
 orgList[i]=orgListMin;
 // Scene 3
 i++;
 orgListMin=[];
 orgListMin.push([800]); // starting pixel
-orgListMin.push([oOrgObjA,300,1050,0,100]);
-orgListMin.push([oOrgObjB,550,1100,0,100]);
-orgListMin.push([oOrgObjC,300,1150,0,100]);
-orgListMin.push([oOrgObjD,400,1200,0,100]);
-orgListMin.push([oOrgObjE,200,1250,0,100]);
-orgListMin.push([oOrgObjF,540,1300,0,100]);
-orgListMin.push([oOrgObjG,300,1350,0,100]);
-orgListMin.push([oOrgObjH,100,1400,0,100]);
-orgListMin.push([oOrgObjI,400,1450,0,100]);
-orgListMin.push([oOrgObjJ,600,1500,0,100]);
+orgListMin.push(['#orgObjA',300,1050,0,100]);
+orgListMin.push(['#orgObjB',550,1100,0,100]);
+orgListMin.push(['#orgObjC',300,1150,0,100]);
+orgListMin.push(['#orgObjD',400,1200,0,100]);
+orgListMin.push(['#orgObjE',200,1250,0,100]);
+orgListMin.push(['#orgObjF',540,1300,0,100]);
+orgListMin.push(['#orgObjG',300,1350,0,100]);
+orgListMin.push(['#orgObjH',100,1400,0,100]);
+orgListMin.push(['#orgObjI',400,1450,0,100]);
+orgListMin.push(['#orgObjJ',600,1500,0,100]);
 orgList[i]=orgListMin;
 // Scene 4
 i++;
 orgListMin=[];
 orgListMin.push([1200]); // starting pixel
-orgListMin.push([oOrgObjB,10,2000,45,100]);
-orgListMin.push([oOrgObjD,10,2000,45,100]);
-orgListMin.push([oOrgObjF,700,2000,-45,100]);
-orgListMin.push([oOrgObjH,700,2000,-45,100]);
-orgListMin.push([oOrgObjJ,600,2000,90,100]);
+orgListMin.push(['#orgObjA',10,2000,45,100]);
+orgListMin.push(['#orgObjB',10,2000,45,100]);
+orgListMin.push(['#orgObjC',700,2000,-45,100]);
+orgListMin.push(['#orgObjI',700,2000,-45,100]);
+orgListMin.push(['#orgObjJ',600,2000,90,100]);
 orgList[i]=orgListMin;
 //
 orgListMin=[];
-
 function fnSceneNum(){
+console.log('fnSceneNum()');
 	var i=0;
 	var j=1;
 	var isTest=0;
 	var tempArr=[];
+	// get the sceen number
 	sceneMin=0;
 	for(i=0;i<orgList.length;i++){
 		if(stageTop>Number(orgList[i][0])){
@@ -104,16 +105,6 @@ function fnSceneNum(){
 	}
 	orgListMin=orgList[sceneMin];
 	orgListMax=orgList[sceneMax];
-	// make sure ListMin doesn't have any extra items that aren't in listMax
-	tempArr=[orgListMin[0]];
-	for(i=1;i<orgListMax.length;i++){
-		for(j=1;j<orgListMin.length;j++){
-			if(orgListMin[j][0]==orgListMax[i][0]){
-				tempArr.push(orgListMin[j]);
-			}
-		}
-	}
-	orgListMin=tempArr;
 	// make sure all of the items in listMax are in ListMin
 	for(i=1;i<orgListMax.length;i++){
 		isTest=0;
@@ -126,6 +117,25 @@ function fnSceneNum(){
 			orgListMin.push(orgListMax[i]);
 		}
 	}
+	// make sure all of the items in listMin are in ListMax
+	for(i=1;i<orgListMin.length;i++){
+		isTest=0;
+		for(var j=1;j<orgListMax.length;j++){
+			if(orgListMin[i][0]==orgListMax[j][0]){
+				isTest=1;
+			}
+		}
+		if(isTest==0){
+			orgListMax.push(orgListMin[i]);
+		}
+	}
+	// remove starting position then sort array, then add starting position back
+	isTest=orgListMin.shift();
+	orgListMin.sort();
+	orgListMin.unshift(isTest); 
+	isTest=orgListMax.shift();
+	orgListMax.sort();
+	orgListMax.unshift(isTest); 
 }
 function setObjects(){
 	stageTop = windowObj.scrollTop();
@@ -133,7 +143,9 @@ function setObjects(){
 		fnSceneNum();
 	}
 	if(stageTop>Number(orgListMax[0])){
-		fnSceneNum();
+		if(sceneMax<orgList.length-1){
+			fnSceneNum();
+		}
 	}
 	var topDiff = Number(orgListMax[0])-Number(orgListMin[0]);
 	var topMove = stageTop-orgListMin[0];
@@ -144,8 +156,8 @@ function setObjects(){
 		percentDiff = 1;
 	}	
 	for(var i=1;i<orgListMin.length;i++){
-		myObj = orgListMin[i][0];
-		// move left
+		myObj = $(orgListMin[i][0]);
+		// move, left, top and opacity
 		myObj.css({"left":(((orgListMax[i][1]-orgListMin[i][1])*percentDiff)+orgListMin[i][1])+'px',"top":(((orgListMax[i][2]-orgListMin[i][2])*percentDiff)+orgListMin[i][2])+'px', 'opacity': (Number((((orgListMax[i][4]-orgListMin[i][4])*percentDiff)+orgListMin[i][4])*.01)),'filter': 'alpha(opacity='+Number(((orgListMax[i][4]-orgListMin[i][4])*percentDiff)+orgListMin[i][4])+')'});
 		// rotate
 		myObj.css({webkitTransform : 'rotate('+(((orgListMax[i][3]-orgListMin[i][3])*percentDiff)+orgListMin[i][3])+'deg)'}); 
@@ -153,9 +165,6 @@ function setObjects(){
 		myObj.css({msTransform     : 'rotate('+(((orgListMax[i][3]-orgListMin[i][3])*percentDiff)+orgListMin[i][3])+'deg)'}); 
 		myObj.css({oTransform      : 'rotate('+(((orgListMax[i][3]-orgListMin[i][3])*percentDiff)+orgListMin[i][3])+'deg)'}); 
 		myObj.css({transform       : 'rotate('+(((orgListMax[i][3]-orgListMin[i][3])*percentDiff)+orgListMin[i][3])+'deg)'}); 
-		// alpha
-		//opacity: 0.4;
-    //filter: alpha(opacity=40);
 	}
 }
 
